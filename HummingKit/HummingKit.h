170f55a89711b67e712051fe01e29afdc260b46c
TonyTang2001 <tonytangdev@outlook.com>
2019-09-02 21:36:34 +0800

    Initial Commit

---
+++ HummingKit/HummingKit.h
@@ -0,0 +1,19 @@
+//
+//  HummingKit.h
+//  HummingKit
+//
+//  Created by 唐子轩 on 2019/9/2.
+//  Copyright © 2019 TonyTang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HummingKit.
+FOUNDATION_EXPORT double HummingKitVersionNumber;
+
+//! Project version string for HummingKit.
+FOUNDATION_EXPORT const unsigned char HummingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HummingKit/PublicHeader.h>
+
+
