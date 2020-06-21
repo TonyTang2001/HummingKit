e393931a39b9cc93c59a2fee9dd958ba0d3e804f
TonyTang2001 <tonytangdev@outlook.com>
2020-06-20 20:11:43 -0700

    Add More Unit Testing Cases - Add test case in fetching catalog album from Apple Music API server.

--- Tests/HummingKitTests/DataModelTests.swift
+++ Tests/HummingKitTests/DataModelTests.swift
@@ -11,14 +11,38 @@ import SwiftyJSON
 
 
 class DataModelTests: XCTestCase {
+    
+    
+    var humming: HummingKit?
+    
 
-//    override func setUpWithError() throws {
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//    }
+    override func setUpWithError() throws {
+        
+//        let usrTokenFetchingExpectation = self.expectation(description: "Apple Music userToken fetching")
 //
-//    override func tearDownWithError() throws {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//    }
+//        HummingKitAuthentication.fetchUserToken(developerToken: devToken) { result in
+//            switch result {
+//            case .success(let token):
+//                self.usrToken = token
+//                self.humming = HummingKit(developerToken: self.devToken, userToken: self.usrToken)
+//                usrTokenFetchingExpectation.fulfill()
+//            case .failure(_):
+//                usrTokenFetchingExpectation.fulfill()
+//                break
+//            }
+//        }
+//
+//        XCTAssertNotEqual(usrToken, "", "Apple Music userToken is not set. Please check code and internet connection.")
+//
+//        waitForExpectations(timeout: 8, handler: nil)
+        
+        self.humming = HummingKit(developerToken: self.devToken, userToken: self.usrToken)
+        
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
 
     func testCatalogSongInitialization() throws {
         // This is an example of a functional test case.
@@ -58,6 +82,25 @@ class DataModelTests: XCTestCase {
         XCTAssertEqual(librarySong?.attributes.name, "7 Minuets, K. 61b: No. 1 in G", "Unexpected Library Song name")
         XCTAssertEqual(librarySong?.attributes.trackNumber, 1, "Unexpected Library Song trackNumber")
         
+    }
+    
+    func testFetchACatalogAlbum() {
+        
+        let catalogAlbumFetchingExpectation = self.expectation(description: "Fetch a catalog album.")
+        
+        humming?.fetchACatalogAlbum(storefront: "us", albumID: "310730204") { result in
+            switch result {
+            case .success(let album):
+                print(album)
+                catalogAlbumFetchingExpectation.fulfill()
+            case .failure(let err):
+                print(err)
+            }
+            
+        }
+        
+        waitForExpectations(timeout: 8, handler: nil)
+
     }
 
 //    func testPerformanceExample() throws {
