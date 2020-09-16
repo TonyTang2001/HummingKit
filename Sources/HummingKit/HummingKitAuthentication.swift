a90e2e2eacbd04c34d5446e97516ca4021fd12f4
TonyTang2001 <tonytangdev@outlook.com>
2020-09-15 21:16:15 -0700

    HummingKitAuthentication Changes - Added function fetchSubscriptionStatus() which asynchronously fetches Apple Music user subscription status. - Improved documentation quality on HummingKitAuthentication and AppleMusicSubscriptionStatus.

--- Sources/HummingKit/HummingKitAuthentication.swift
+++ Sources/HummingKit/HummingKitAuthentication.swift
@@ -9,43 +9,75 @@
 import Foundation
 import StoreKit
 
+@available(iOS 11.0, macOS 11.0, watchOS 7.0, macCatalyst 13.0, *)
+/// Get essential Apple Music API's authentication information and tokens and user's subscription status
 public struct HummingKitAuthentication {
     
-    /// Static function for fetching Apple Music user token from server using developer token.
+    /// Fetch Apple Music user token from server using developer token.
     /// - Parameters:
     ///   - developerToken: Developer Token provided by the developer who use this package, required for authentication.
-    ///   - completion: Result<String, Error> containing request status and token returned by server.
+    ///   - completion: Completion handler of request result of type Result<String, Error>, containing request status and token returned by server.
+    ///   - result: Result of request, .success(userToken) or .failure(Error)
     public static func fetchUserToken(developerToken: String,
-                                      completion: @escaping (Result<String, Error>) -> Void) {
+                                      completion: @escaping (_ result: Result<String, Error>) -> Void) {
         
-        var result: Result<String, Error> = .success("") // initialzation required before closure
-        
-        if #available(iOS 11.0, macOS 11.0, watchOS 7.0, macCatalyst 13.0, *) {
-            // SKCloudServiceController() is currently unavailable in macOS
-            #if !os(macOS)
-            let controller = SKCloudServiceController()
-            controller.requestUserToken(forDeveloperToken: developerToken) { (userToken: String?, error: Error?) in
-                if let musicUserToken = userToken {
-                    // Fetching SUCCEEDED
-                    result = .success(musicUserToken)
-                    completion(result)
-                } else {
-                    // Fetching FAILED
-                    guard let error = error else { return }
-                    result = .failure(error)
-                    completion(result)
-                }
+        // SKCloudServiceController() is unavailable in macOS
+        #if !os(macOS)
+        let controller = SKCloudServiceController()
+        controller.requestUserToken(forDeveloperToken: developerToken) { (userToken: String?, error: Error?) in
+            if let musicUserToken = userToken {
+                // Fetching SUCCEEDED
+                completion(.success(musicUserToken))
+            } else {
+                // Fetching FAILED
+                guard let error = error else { return }
+                completion(.failure(error))
             }
-            #endif
-        } else {
-            // return failure if iOS version is too low to support functionality
-            result = .failure(HummingKitEnvironmentError.systemVersionOutdated)
-            completion(result)
         }
+        #endif
     }
     
-    
-    
+    /// Fetch Apple Music User Subscription Status.
+    /// - Parameters:
+    ///   - completion: Completion handler of request result of type Result<[AppleMusicSubscriptionStatus], Error>, containing request status and AppleMusicSubscriptionStatus returned by server.
+    ///   - result: Result of request, .success([AppleMusicSubscriptionStatus]) or .failure(Error)
+    public static func fetchSubscriptionStatus(completion: @escaping (_ result: Result<[AppleMusicSubscriptionStatus], Error>) -> Void) {
+        
+        let controller = SKCloudServiceController()
+        controller.requestCapabilities { (capabilities: SKCloudServiceCapability, error: Error?) in
+            let canPlay = capabilities.contains(.musicCatalogPlayback)
+            let canAdd = capabilities.contains(.addToCloudMusicLibrary)
+            let canSubscribe = capabilities.contains(.musicCatalogSubscriptionEligible)
+            
+            if let err = error {
+                // Error occured
+                completion(.failure(err))
+            } else {
+                // Request succeeded
+                var status: [AppleMusicSubscriptionStatus] = []
+                if canPlay {
+                    status.append(.canPlay)
+                }
+                if canAdd {
+                    status.append(.canAdd)
+                }
+                if canSubscribe {
+                    status.append(.canSubscribe)
+                }
+                completion(.success(status))
+            }
+        }
+        
+    }
     
 }
 
+/// User's Apple Music subscription status on current device as may change under different settings or parental controls
+public enum AppleMusicSubscriptionStatus: CaseIterable, Comparable {
+    /// The device allows playback of Apple Music catalog tracks.
+    case canPlay
+    /// The device allows tracks to be added to the user’s music library.
+    case canAdd
+    /// The device allows subscription to the Apple Music catalog.
+    case canSubscribe
+}
